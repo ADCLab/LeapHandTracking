2403f4c600317ff03292c672d7cbe1d8a21f3776
sewell <sewelljohnathan@outlook.com>
2023-03-16 13:50:53 -0400

    Add batch files for compilation and running

---
+++ compile.bat
@@ -0,0 +1,14 @@
+
+@echo off
+
+REM Set variables
+SET CUR_DIR=%cd%
+SET SRC_DIR=%CUR_DIR%/realtimehandtracking/
+SET BUILD_TYPE=Release
+SET REPOS_BUILD_ROOT=%CUR_DIR%/build
+SET REPOS_INSTALL_ROOT=%CUR_DIR%/exe
+
+REM Run cmake
+cmake -S %SRC_DIR% -B %REPOS_BUILD_ROOT%/%BUILD_TYPE%/LeapSDK/leapc_example
+    
+cmake --build %REPOS_BUILD_ROOT%/%BUILD_TYPE%/LeapSDK/leapc_example -j --config %BUILD_TYPE%    
---
+++ run.bat
@@ -0,0 +1,4 @@
+
+@echo off
+SET CUR_DIR=%cd%
+%CUR_DIR%\build\Release\LeapSDK\leapc_example\Release\RealTimeHandTracking.exe %1
