fa3a941371ed3464e9f51ea70d42cd2d4c3c240e
sewell <sewelljohnathan@proton.me>
2023-07-12 19:42:58 -0400

    test

--- realtimehandtracking/realtimehandtracking.c
+++ realtimehandtracking/realtimehandtracking.c
@@ -47,6 +47,15 @@ int gettimeofday(struct timeval* tp, struct timezone* tzp)
 
 FILE *logFile;
 Plane screenPlane;
+int isCalibrated = 0;
+typedef struct {
+    unsigned long long milliseconds;
+    LEAP_TRACKING_EVENT frame;
+} TimedFrame;
+
+TimedFrame* calibrateFrames;
+int calibrateFramesSize = 0;
+int calibrateFramesMax = 16;
 
  /** Callback for when the connection opens. */
 static void OnConnect() {
@@ -58,8 +67,30 @@ static void OnDevice(const LEAP_DEVICE_INFO* props) {
 }
 /** Callback for when a frame of tracking data is available. */
 static void OnFrame(const LEAP_TRACKING_EVENT* frame) {
-    if (frame->info.frame_id % 60 == 0)
+    if (frame->info.frame_id % 60 == 0) {
         printf("Frame %lli with %i hands.\n", (long long int)frame->info.frame_id, frame->nHands);
+    }
+
+    if (!isCalibrated) {
+        if (frame->info.frame_id % 6 == 0 && frame->nHands > 0) {
+            struct timeval ts;
+            gettimeofday(&ts, NULL); // return value can be ignored
+
+            TimedFrame timedFrame;
+            timedFrame.frame = *frame;
+            timedFrame.milliseconds = (unsigned long long) (ts.tv_sec) * 1000 + (unsigned long long) (ts.tv_usec) / 1000;
+
+            if (calibrateFramesSize == calibrateFramesMax) {
+                realloc(calibrateFrames, sizeof(TimedFrame) * (calibrateFramesMax *= 2));
+            }
+
+            calibrateFrames[calibrateFramesSize++] = timedFrame;
+        }
+
+        return;
+    } 
+
+    
 
     for (uint32_t h = 0; h < frame->nHands; h++) {
         LEAP_HAND* hand = &frame->pHands[h];
@@ -164,11 +195,83 @@ int getFlaskData(flaskData* data) {
     return 0;
 }
 
-int main(int argc, char** argv) {
+int calibrate() {
 
-    // Get the plane from the calibration
-    FILE* calibrateFile = fopen("calibrate.txt", "r");
-    int res = fscanf(calibrateFile, "%f%f%f%f", &screenPlane.a, &screenPlane.b, &screenPlane.c, &screenPlane.d);
+    FILE* calibrateFile;
+    while ((calibrateFile = fopen("calibrate.txt", "r")) == NULL) { sleep(0.01); };
+    isCalibrated = 1;
+
+    // Get the number of points
+    uint32_t calibrateTimesCount;
+    if (fscanf(calibrateFile, "%d", &calibrateTimesCount) == 0) {
+        printf("Invalid calibrate.txt format\n");
+        return 1;
+    }
+
+    // Get the timestamps of the points
+    unsigned long long calibrateTimes[calibrateTimesCount];
+    for (uint32_t i = 0; i < calibrateTimesCount; i++) {
+        if (fscanf(calibrateFile, "%llu", &calibrateTimes[i]) == 0) {
+            printf("Invalid calibrate.txt format\n");
+            return 1;
+        }
+        printf("%llu\n", calibrateTimes[i]);
+    }
+
+    fclose(calibrateFile);
+
+    // Get the cooresponding vectors
+    Vector calibratePoints[calibrateTimesCount];
+    uint32_t calibrateTimeIndex = 0;
+    for (uint32_t i = 0; i < calibrateFramesSize - 1; i++) {
+
+        if (calibrateTimeIndex == calibrateTimesCount) {
+            break;
+        }
+
+        // Check if the next timestamp is between the next set of frames
+        unsigned long long currentTime = calibrateTimes[calibrateTimeIndex];
+        LEAP_TRACKING_EVENT frame = calibrateFrames[i].frame;
+        if (currentTime >= calibrateFrames[i].milliseconds && currentTime < calibrateFrames[i+1].milliseconds) {
+
+            LEAP_HAND hand = calibrateFrames[i].frame.pHands[0];
+            
+            // Get the furthest digit
+            int furthestDigit = 0;
+            float furthestDistance = 0;
+            for (uint32_t digit = 0; digit < 5; digit++) {
+                
+                float distance = sqrt(
+                    pow(hand.palm.position.x - hand.digits[digit].distal.next_joint.x, 2) +
+                    pow(hand.palm.position.y - hand.digits[digit].distal.next_joint.y, 2) +
+                    pow(hand.palm.position.z - hand.digits[digit].distal.next_joint.z, 2)
+                );
+
+                if (distance > furthestDistance) {
+                    furthestDigit = digit;
+                    distance = furthestDistance;
+                } 
+
+            }
+
+            calibratePoints[calibrateTimeIndex].x = hand.digits[furthestDigit].distal.next_joint.x;
+            calibratePoints[calibrateTimeIndex].y = hand.digits[furthestDigit].distal.next_joint.y;
+            calibratePoints[calibrateTimeIndex].z = hand.digits[furthestDigit].distal.next_joint.z;
+
+        }
+
+        calibrateTimeIndex++;
+    }
+
+    for (uint32_t i = 0; i < calibrateTimesCount; i++) {
+        printf("(%f, %f, %f)\n", calibratePoints[i].x, calibratePoints[i].y, calibratePoints[i].z);
+    }
+
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
 
     // Get the date
     char date[100] = "";
@@ -184,16 +287,33 @@ int main(int argc, char** argv) {
     sprintf(filename, "%s_%s_Hands_%s.log", data.userId, date, data.studyStage);
     logFile = fopen(filename, "w");
 
+    // Malloc
+    calibrateFrames = (TimedFrame*) malloc(10 * sizeof(TimedFrame));
+
     //Set callback function pointers
     ConnectionCallbacks.on_connection = &OnConnect;
     ConnectionCallbacks.on_device_found = &OnDevice;
     ConnectionCallbacks.on_frame = &OnFrame;
     LEAP_CONNECTION* connection = OpenConnection();
     LeapSetPolicyFlags(*connection, eLeapPolicyFlag_Images, 0);
-    printf("Press Enter to exit program.\n");
-    getchar();
+
+    
+    char path[200];
+    getcwd(path, sizeof(path));
+    char command[250];
+    sprintf(command, "firefox %s/realtimehandtracking/calibrate.html", path);
+    system(command);
+    
+    // Get the plane from the calibration
+    if (calibrate()) {
+        return 1;
+    }
+    
+    exit(1);
 
     // Cleanup
+    printf("Press Enter to exit program.\n");
+    getchar();
     fclose(logFile);
     CloseConnection();
     DestroyConnection();
